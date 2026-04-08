7e3835620712329ed785addfc2ee335d58070bf1
shreyaa-kulkarni <shreyaakulkarni2006@gmail.com>
2026-04-09 00:49:04 +0530

    Update main.py

--- webapp/main.py
+++ webapp/main.py
@@ -4,4 +4,4 @@ app=Flask(__name__)
 def home():
   return render_templat('index.html',title="home page")
   if __name__=='__main__':
-    app.un(host='127.0.0.1',port=8080,debug=True)
+    app.run(host='127.0.0.1',port=8080,debug=True)
