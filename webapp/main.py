ebd153020c576355e7ff919038500f44c713dae9
shreyaa-kulkarni <shreyaakulkarni2006@gmail.com>
2026-04-08 13:04:01 +0530

    Create main.py

---
+++ webapp/main.py
@@ -0,0 +1,7 @@
+from flask import Flask,render_template
+app=Flsk(__name__)
+@app.route('/')
+def home():
+  return render_templat('index.html',title="home page")
+  if __name__=='__main__':
+    app.un(host='127.0.0.1',port=8080,debug=True)
